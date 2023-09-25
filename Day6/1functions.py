125ce6a637d74694cdb63fc93fb0a68cf066c1d9
sahilotari <sahilotari3@gmail.com>
2023-09-25 15:34:00 +0530

    Day 6 Added

---
+++ Day6/1functions.py
@@ -0,0 +1,15 @@
+print("Hello")
+numChar = len("Hello")
+print(numChar)
+
+# def <function_name>():
+#     function body
+
+
+def my_func():
+    print("Hello")
+    print("Bye")
+
+
+# calling functions
+my_func()
