9e0b1e8ceb996c279424d9b539d429debd19e23d
sahilotari <sahilotari3@gmail.com>
2023-10-03 23:19:22 +0530

    using external packages

--- Day16/OOPSdemo.py
+++ Day16/OOPSdemo.py
@@ -1,15 +1,29 @@
-import turtle
+# import turtle
 
-# creating objects
-timmy = turtle.Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
+# # creating objects
+# timmy = turtle.Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
 
-# object attributes
-my_screen = turtle.Screen()
-print(my_screen.canvheight)
+# # object attributes
+# my_screen = turtle.Screen()
+# print(my_screen.canvheight)
 
 
-# object methods
-my_screen.exitonclick()
+# # object methods
+# my_screen.exitonclick()
+
+
+# use external packages
+# install them using pip : python -m pip install prettytable
+# use them
+
+import prettytable
+
+table = prettytable.PrettyTable()
+# print(table)
+table.align = "l"
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+print(table)
