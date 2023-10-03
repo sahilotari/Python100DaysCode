a73934c77ddf34a4c8678acec8e3cbfeac32c4ff
sahilotari <sahilotari3@gmail.com>
2023-10-03 22:38:25 +0530

    OOP demo using turtle

---
+++ Day16/OOPSdemo.py
@@ -0,0 +1,15 @@
+import turtle
+
+# creating objects
+timmy = turtle.Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+
+# object attributes
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+
+
+# object methods
+my_screen.exitonclick()
